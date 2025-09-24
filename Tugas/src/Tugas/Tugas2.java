4461fda6a02fdbc528ecc5a39f54db742c717a20
Luthfion <78730006+MakotoKirishima@users.noreply.github.com>
2025-09-24 22:19:35 +0700

    Eksepsi

---
+++ Tugas/src/Tugas/Tugas2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tugas;
+
+/**
+ *
+ * @author Alose
+ */
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Tugas2 {
+
+    public static void main(String[] args) {
+        try (FileReader fr = new FileReader("data.txt")) {
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            int angka = Integer.parseInt(line); // Perbandingan dengan metode BufferedWriter baru
+            System.out.println("Hasil: " + (10 / angka));
+        } catch (FileNotFoundException e) {
+            System.out.println("Terjadi kesalahan: File tidak ditemukan");
+        } catch (IOException e) {
+            System.out.println("Gagal membaca file: " + e.getMessage());
+        }
+    }
+}
